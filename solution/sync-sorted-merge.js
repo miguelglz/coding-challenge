dc801d888a4563f3910ccb4de45b2f2aea1211a2
Miguel González <mglz06@gmail.com>
2021-02-04 12:33:54 -0600

    FEAT|added sync sort functionality

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,31 @@
 "use strict";
-
 // Print all entries, across all of the sources, in chronological order.
 
+function getPositionIndex(array, value) {
+  let low = 0;
+  let high = array.length;
+
+  while (low < high) {
+    let mid = (low + high) >>> 1;
+    if (array[mid].date < value) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+  }
+  return low;
+}
+
 module.exports = (logSources, printer) => {
-  return console.log("Sync sort complete.");
+  const logEntries = [];
+  let logEntry = logSources.pop();
+  while (logEntry) {
+    const newIndex = getPositionIndex(logEntries, logEntry.last.date);
+    logEntries.splice(newIndex, 0, logEntry.last);
+    logEntry = logSources.pop();
+  }
+  logEntries.forEach((logEntry) => {
+    printer.print(logEntry);
+  });
+  return printer.done();
 };
